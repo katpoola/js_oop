35b0c0a4a74616384b611ae7ecc245552e619685
Katriin Poolakese <katriin.poolaese@khk.ee>
2021-10-15 15:31:15 +0300

    OOJS - 1

---
+++ OOJS.js
@@ -0,0 +1,17 @@
+function Shape(name, sides, sideLength) {
+	this.name = name;
+	this.sides = sides;
+	this.sideLength = sideLength;
+	this.calcPerimeter = function() {
+		return this.sides * this.sideLength;
+	}
+}
+
+
+let square = new Shape('square', 4, 5);
+let triangle = new Shape('triangle', 3, 3);
+
+console.log(triangle);
+console.log(triangle.calcPerimeter());
+console.log(square);
+console.log(square.calcPerimeter());
